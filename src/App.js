c912e3760a9f0b423873ad4d8b61391e2de6ecae
caitlin <caitlin.welsh9@gmail.com>
2018-05-13 19:42:30 -0600

    fixed merge conflict in roomlist.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-
-=======
 import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 import RoomList from './components/RoomList';
 
-var config = {
-    apiKey: "AIzaSyB9owGLvUlEF-jrYLxD54td1ISZlizXbho",
-    authDomain: "bloc-web-chat.firebaseapp.com",
-    databaseURL: "https://bloc-web-chat.firebaseio.com",
-    projectId: "bloc-web-chat",
-    storageBucket: "bloc-web-chat.appspot.com",
-    messagingSenderId: "935032876915"
-  };
-  firebase.initializeApp(config);
-
 class App extends Component {
->>>>>>> e9de80c150b7a54f91834802555a772134cf0bca
 
   constructor(props) {
     super(props);
@@ -32,13 +14,7 @@ class App extends Component {
   }
 
   componentWillMount() {
-<<<<<<< HEAD
 
-=======
-    const self = this;
-    const url = "https://bloc-web-chat.firebaseio.com";
-    this.roomsRef = this.props.firebase.database().ref('rooms');
->>>>>>> e9de80c150b7a54f91834802555a772134cf0bca
   }
 
   render() {
