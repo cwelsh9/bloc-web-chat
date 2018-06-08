e764d7df9e518004851fc73a009a63da35f260b9
caitlin <caitlin.welsh9@gmail.com>
2018-06-08 15:07:45 -0600

    wip: getting buttons to work

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import MessageList from './MessageList.js'
+import ChatInput from './ChatInput.js'
 
 class ChatApp extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = { messages: [] };
+    this.sendHandler = this.sendHandler.bind(this);
   }
 
   sendHandler(message) {
@@ -12,6 +15,29 @@ class ChatApp extends React.Component {
       user: this.props.user,
       message
     };
+
+    messageObject.fromMe = true;
+    this.addMessage(messageObject);
   }
 
+  addMessage(message) {
+    const messages = this.state.MessageList;
+    messages.push(message);
+    this.setState({ MessageList });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Bloc Web Chat</h3>
+        <MessageList messages={this.state.messages} />
+        <ChatInput onSend={this.sendHandler} />
+      </div>
+    );
+  }
 }
+  ChatApp.defaultProps = {
+    user: 'Anonymous'
+  };
+
+  export default ChatApp;
